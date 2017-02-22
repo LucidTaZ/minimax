0339c4311b7cc20e8d9b7e24e61547bc5ce12f5f
Thijs Zumbrink <thijs@targetmedia.nl>
2017-02-22 16:27:37 +0100

    Explain why the tests use inheritance instead of a Decorator

--- tests/tictactoe/ShuffledDecisionsGameState.php
+++ tests/tictactoe/ShuffledDecisionsGameState.php
@@ -7,6 +7,17 @@ namespace lucidtaz\minimax\tests\tictactoe;
  *
  * This is to rule out any tests that accidentally succeed because of
  * coincidence.
+ *
+ * Note that this approach uses inheritance rather than applying the arguably
+ * more sensible Decorator pattern. The reason for this is that the code
+ * (currently, perhaps it will be solved) it not really strict with regards to
+ * typing, and will call methods on the TicTacToe GameState class that are not
+ * defined in the GameState interface, such as makeMove(). Until such issues are
+ * resolved by an interface redesign, we must resort to inheritance.
+ *
+ * Furthermore, a decorated object may lose its decoration when passing out
+ * references of itself to other code. This makes the pattern more hassle than
+ * it's worth.
  */
 class ShuffledDecisionsGameState extends GameState
 {
