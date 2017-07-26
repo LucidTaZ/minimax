4fe82f8c71bb1c99d17c47c46cb7cb79a4029422
Thijs Zumbrink <thijs@targetmedia.nl>
2017-07-26 20:10:27 +0200

    Verify optimization with timed test case

--- tests/ReversiTest.php
+++ tests/ReversiTest.php
@@ -70,4 +70,47 @@ class ReversiTest extends TestCase
         $this->assertEquals(1, $newState->board->countOwnedCells($RED), 'Red cell count decreased (piece captured)');
         $this->assertEquals($RED, $newState->getNextPlayer(), 'Player turn proceeded');
     }
+
+    /**
+     * Verify alpha-beta pruning optimization
+     *
+     * Empyrical test times:
+     * Max depth
+     *       | Duration normal
+     *       |       | Duration AB pruning
+     * ------+-------+---
+     *     1 |   0.0 |  0.0
+     *     2 |   0.0 |  0.0
+     *     3 |   0.1 |  0.2
+     *     4 |   0.6 |  0.5
+     *     5 |   3   |  1
+     *     6 |  20   |  5
+     *     7 | 165   | 17
+     *
+     * @medium
+     * @todo For determinism it's better to inspect the number of visited nodes
+     */
+    public function testEngineHandlesLargeSearchSpace()
+    {
+        $BLUE = Player::BLUE();
+        $RED = Player::RED();
+        $maxDepth = 6;
+
+        $state = new GameState();
+        $this->assertEquals(2, $state->board->countOwnedCells($BLUE), 'Test precondition');
+        $this->assertEquals(2, $state->board->countOwnedCells($RED), 'Test precondition');
+        $this->assertEquals($BLUE, $state->getNextPlayer(), 'Test precondition');
+
+        $startTime = microtime(true);
+        $engine = new Engine($BLUE, $maxDepth);
+        /* @var $newState GameState */
+        $newState = $engine->decide($state);
+        $duration = microtime(true) - $startTime;
+
+        $this->assertEquals(4, $newState->board->countOwnedCells($BLUE), 'Blue cell count increased');
+        $this->assertEquals(1, $newState->board->countOwnedCells($RED), 'Red cell count decreased (piece captured)');
+        $this->assertEquals($RED, $newState->getNextPlayer(), 'Player turn proceeded');
+
+        $this->assertLessThan(10, $duration, 'Search duration must be short enough');
+    }
 }
